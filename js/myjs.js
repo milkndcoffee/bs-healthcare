88166cf9ab8d478cb62dc0908a4082b68a351979
milkndcoffee <milkndcoffee@gmail.com>
2020-09-02 19:01:26 -0500

    Updates

--- js/myjs.js
+++ js/myjs.js
@@ -2,6 +2,7 @@
 //sets up our functions
 function scrollNav(){
   var navbar = document.getElementsByClassName("navbar");
+  var mainDOM = document.getElementsByTagName("MAIN")[0];
   navbar = navbar[0];
   console.log(navbar);
 
@@ -10,9 +11,14 @@ function scrollNav(){
   window.onscroll = function() {
     if (window.pageYOffset >= y) {
       navbar.classList.add("sticky-this");
+      mainDOM.classList.add("push-up"); //smooth's the sticky when Y is met
     } else {
       navbar.classList.remove("sticky-this");
+      mainDOM.classList.remove("push-up");
     }
   };
 }
 
+const alertButton = () => {
+  window.alert("Hi, this page was made with the intention of just the 'display' and as such, back-end functions will not be implemented (like this form submission).");
+}
